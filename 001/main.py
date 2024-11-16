5aa437c61c198791379f7d6aa768b2deff8df1d3
Jose Leitao <jose.p.leitao@gmail.com>
2024-11-16 16:49:08 -0300

    First Commit

---
+++ 001/main.py
@@ -0,0 +1,7 @@
+# Author: Jose P. Leitao
+# 2024-11-16
+# This program is use to trainning IA with Python
+# based on "Complete Data Science & Machine Learning Bootcamp - Python 3" Course
+# Objective: Load a csv file and display the data using pandas and matplot
+
+import pandas

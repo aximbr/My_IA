c988059050deddee4b722007cf6d7105b52e0625
Jose Leitao <jose.p.leitao@gmail.com>
2024-11-20 09:43:02 -0300

    Lesson 3.18

---
+++ 004/main.py
@@ -0,0 +1,12 @@
+"""
+# Author: Jose P. Leitao
+# 2024-11-20
+# This program is used to trainning IA with Python
+# based on "Complete Data Science & Machine Learning Bootcamp - Python 3" Course
+# Lesson 3.18
+# Objective: exercise with Python
+"""
+
+import pandas as pd
+data = pd.read_csv('lsd_math_score_data.csv')
+

18f4b9daea4fd602eedfab872c2b879d2ffc7cb1
Jose P. Leitao <jose.p.leitao@gmail.com>
2025-01-22 22:08:30 -0300

    Lesson 85 end of chapter.

--- 020/boston_valuation.py
+++ 020/boston_valuation.py
@@ -1,4 +1,3 @@
-#from sklearn.datasets import load_boston
 from sklearn.datasets import fetch_openml
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -7,69 +6,63 @@ import pandas as pd
 import numpy as np
 
 # Gather Data
-#boston_dataset = load_boston()
+
 boston_dataset = fetch_openml(name='boston', version=1)
 data = pd.DataFrame(data=boston_dataset.data, columns=boston_dataset.feature_names)
 features = data.drop(['INDUS', 'AGE'], axis=1)
+
 features['CHAS'] = features['CHAS'].astype('float64')
 features['RAD'] = features['RAD'].astype('float64')
 
 log_prices = np.log(boston_dataset.target).to_numpy()
 # add to_numpy() method to force return a Array otherwise returns a serie
-#print(type(log_prices))
+
 target = pd.DataFrame(log_prices, columns=['PRICE'])
-#print(target)
-CRIME_IDX = 0
-ZN_IDX = 1
-CHAS_IDX = 2
-RM_IDX = 4
-PTRATIO_IDX = 8
 
-ZILLOW_MEDIAN_PRICE = 583.3
-SCALE_FACTOR = ZILLOW_MEDIAN_PRICE / np.median(boston_dataset.target)
-
-property_stats = features.mean().values.reshape(1, 11)
-
-#print(property_stats)
+#mean values
+property_stats = features.mean().to_frame().transpose()
 
 regr = LinearRegression().fit(features, target)
 fitted_vals = regr.predict(features)
 
-MSE = mean_squared_error(target, fitted_vals)
+
+MSE = mean_squared_error(target, fitted_vals )
 RMSE = np.sqrt(MSE)
 
+ZILLOW_MEDIAN_PRICE = 583.3
+SCALE_FACTOR = ZILLOW_MEDIAN_PRICE /np.median(boston_dataset.target)
 
-def get_log_estimate(nr_rooms,
-                     students_per_classroom,
-                     next_to_river=False,
-                     high_confidence=True):
 
-    # Configure property
-    property_stats[0][RM_IDX] = nr_rooms
-    property_stats[0][PTRATIO_IDX] = students_per_classroom
-
+def get_log_estimate(nr_rooms, students_per_classroom, 
+                     next_to_river=False, 
+                     high_confidence=False):
+    # Configure Property
+    property_stats['RM'] = nr_rooms
+    property_stats['PTRATIO'] = students_per_classroom
     if next_to_river:
-        property_stats[0][CHAS_IDX] = 1
+        property_stats['CHAS'] = 1
     else:
-        property_stats[0][CHAS_IDX] = 0
+        property_stats['CHAS'] = 0
 
     # Make prediction
-    log_estimate = regr.predict(property_stats)[0][0]
+    log_estimate = regr.predict(property_stats)
 
-    # Calc Range
+    # Calc range
     if high_confidence:
-        upper_bound = log_estimate + 2 * RMSE
-        lower_bound = log_estimate - 2 * RMSE
+        # calc 2 ST. D
+        upper_bound = log_estimate + 2* RMSE
+        lower_bound = log_estimate - 2* RMSE
         interval = 95
     else:
+        # calc 1 ST. D.
+        # calc 2 ST. D
         upper_bound = log_estimate + RMSE
         lower_bound = log_estimate - RMSE
         interval = 68
-
+    
     return log_estimate, upper_bound, lower_bound, interval
 
-
-def get_dollar_estimate(rm, ptratio, chas=False, large_range=True):
+def get_dollar_estimate(rm, ptratio, chas=False, range=True):
     """Estimate the price of a property in Boston.
 
     Keyword arguments:
@@ -80,25 +73,24 @@ def get_dollar_estimate(rm, ptratio, chas=False, large_range=True):
 
     """
 
-    if rm < 1 or ptratio < 1:
-        print('That is unrealistic. Try again.')
+    if rm < 1 or ptratio < 1 :
+        print('That is unrealistic! Try again.')
         return
+    
 
-    log_est, upper, lower, conf = get_log_estimate(rm,
-                                                   students_per_classroom=ptratio,
+    log_est, upper, lower, conf = get_log_estimate(rm, ptratio,
                                                    next_to_river=chas,
-                                                   high_confidence=large_range)
+                                                    high_confidence=range )
 
-    # Convert to today's collars
-    dollar_est = np.e ** log_est * 1000 * SCALE_FACTOR
-    dollar_hi = np.e ** upper * 1000 * SCALE_FACTOR
-    dollar_low = np.e ** lower * 1000 * SCALE_FACTOR
+    # convert to today's dollar
+    dollar_est = np.e**(log_est) * 1000 * SCALE_FACTOR
+    dollar_up = np.e**(upper) * 1000 * SCALE_FACTOR
+    dollar_low = np.e**(lower) * 1000 * SCALE_FACTOR
 
-    # Round the dollar values to nearest thousand
-    rounded_est = np.around(dollar_est, -3)
-    rounded_hi = np.around(dollar_hi, -3)
-    rounded_low = np.around(dollar_low, -3)
+    rounded_est = np.around(dollar_est,-3)
+    rounded_up = np.around(dollar_up,-3)
+    rounded_low = np.around(dollar_low,-3)
 
-    print(f"The estimated property value is {rounded_est}.")
-    print(f"At {conf}% confidence the valuation range is")
-    print(f"USD {rounded_low} at the lower end to USD {rounded_hi} at the high end.")
+    print(f'The estimate property value is USD {rounded_est}')
+    print(f'At {conf}% confidence of valuation range is')
+    print(f'USD {rounded_low} at the lower end to USD {rounded_up} at higher end')
